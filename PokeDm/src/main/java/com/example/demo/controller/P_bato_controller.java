2c0e32725aed7769e3a4666b1a8d7a1d8b6e22c0
KA-TETO <kazuki.a.1613@gmail.com>
2023-11-24 10:06:31 +0900

    プロトタイプ作成

--- PokeDm/src/main/java/com/example/demo/controller/P_bato_controller.java
+++ PokeDm/src/main/java/com/example/demo/controller/P_bato_controller.java
@@ -1,13 +1,39 @@
 package com.example.demo.controller;
 
-import org.springframework.ui.Model;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class P_bato_controller {
 	
+		@Autowired
+		JdbcTemplate jdbcTemplate;
+		
+	@RequestMapping(path="/poke_damage",method=RequestMethod.GET)
 	public String battle(Model model) {
-		System.out.print("");
+		
 		return "p_battle" ;
 		
+	}
+	@RequestMapping(path="/poke_damage",method=RequestMethod.POST)
+	public String battle_gra(String Poke_name1,String Poke_name2,Model model) {
+		List<Map<String,Object>> resultList;
+		
+		if(Poke_name1!=null) {
+			resultList=jdbcTemplate.queryForList("select * from demo where name =?",Poke_name1);
+			model.addAttribute("selectResult",resultList);
+		}else if(Poke_name2!=null) {
+			resultList=jdbcTemplate.queryForList("select * from demo where name=? ",Poke_name2);
+			model.addAttribute("selectResult",resultList);
+		}
+		return "p_battle";
 		
 	}
 
